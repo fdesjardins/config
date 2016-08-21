db629b499d9be22627b7dd776054a1845b440fc1
Forrest Desjardins <desjardinsfg@gmail.com>
2016-08-21 03:48:13 -0800

    Add oh-my-zsh install

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ws="$HOME/Workspace"
 mkdir -p $ws && cd $ws
 git clone --recursive git://github.com/fdesjardins/config.git
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 bash config/dotfiles/sync.sh

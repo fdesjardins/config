e71e9b8bcd23c56bb93cf9ed2dab1e0cb0db52a0
Forrest Desjardins <desjardinsfg@gmail.com>
2016-08-21 22:54:34 -0800

    Add global npm packages and config

--- dotfiles/.zsh/environment.zsh
+++ dotfiles/.zsh/environment.zsh
@@ -1,23 +1,23 @@
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/sbin:/usr/local/bin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/sbin:/usr/local/bin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export PATH=$PATH:$HOME/Workspace/bin
-export PATH=$PATH:$HOME/Workspace/nodejs/bin
-export PATH=$PATH:$HOME/Workspace/nodejs/n
-export PATH=$PATH:$HOME/.rvm/bin
-export PATH=$PATH:$HOME/Workspace/go/bin
 
+# Term
+export TERM='xterm-256color'
 export ZSH=$HOME/.oh-my-zsh
 
-ZSH_CACHE_DIR=$HOME/.oh-my-zsh-cache
-if [[ ! -d $ZSH_CACHE_DIR ]]; then
-  mkdir $ZSH_CACHE_DIR
-fi
+# NodeJS
+export NPM_PACKAGES="${HOME}/.npm-packages"
+export PATH=$NPM_PACKAGES/bin:$PATH
 
-export TERM='xterm-256color'
+# Ruby
+export PATH=$PATH:$HOME/.rvm/bin
 
-export ANSIBLE_NOCOWS=1
-
-export N_PREFIX=$HOME/Workspace/nodejs
-
+# Go
+export PATH=$PATH:$HOME/Workspace/go/bin
 export GOPATH=$HOME/Workspace/go
 
+# Python
 export WORKON_HOME=$HOME/.virtualenvs
+
+# Misc
+export ANSIBLE_NOCOWS=1
--- system/install-dev-packages.sh
+++ system/install-dev-packages.sh
@@ -2,17 +2,43 @@
 
 # add NodeJS apt source
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt update
 
 packages=(
   "atom"
   "deluge"
+  "docker.io"
   "fonts-inconsolata"
   "gimp"
   "google-chrome-stable"
   "inkscape"
   "nginx"
   "nodejs"
+  "nodejs-legacy"
+  "npm"
+  "virtualbox"
   "vlc"
 )
 
 sudo apt install -y "${packages[@]}"
+
+# place global npm packages in home directory
+# https://github.com/sindresorhus/guides/blob/master/npm-global-without-sudo.md
+mkdir "${HOME}/.npm-packages"
+
+# global NodeJS packages
+npmPackages=(
+  "eslint"
+  "generator-nm"
+  "gulp"
+  "loadtest"
+  "np"
+  "npm"
+  "npm-check-updates"
+  "pm2"
+  "public-ip-cli"
+  "speed-test"
+  "yo"
+)
+
+npm install -g "${npmPackages[@]}"

8daa53b24f86c5d564a11c7a5e759c66ba24c82b
Forrest Desjardins <desjardinsfg@gmail.com>
2017-04-06 06:00:01 -0800

    sync

--- dotfiles/.zsh/startup.zsh
+++ dotfiles/.zsh/startup.zsh
@@ -5,6 +5,23 @@ P=(
   'Steven Wright'
   'Bill Nye'
   'Elon Musk'
+  'Stephen King'
+  'Vladimir Putin'
+  'Bill Gates'
+  'Warren Buffet'
+  'Nelson Mandela'
+  'Bernie Sanders'
+  'Richard Feynman'
+  'Albert Einstein'
+  'Donald Knuth'
+  'Alan Turing'
+  'Mary Lasker'
+  'Jim Lovell'
+  'Ben Bernanke'
+  'Joe Biden'
+  'Tom Hanks'
+  'Elie Wiesel'
+  'Neil Degrasse Tyson'
 )
 
 wikiquote random ${P[$(( $RANDOM % ${#P[@]} + 1 ))]} | cowsay

bccad061383b151b7468c2f79b5d0a79ac78bdf2
Forrest Desjardins <desjardinsfg@gmail.com>
2017-04-26 06:00:01 -0800

    sync

--- dotfiles/.zsh/aliases.zsh
+++ dotfiles/.zsh/aliases.zsh
@@ -44,3 +44,5 @@ alias wikiquote="node /home/forrest/Workspace/npm/wikiquote-cli/index.js"
 
 alias mknm="cpv ~/Workspace/etc/nm/{.*,*}"
 alias syncconfig="cd ~/Workspace/config/ && git add . --all && git commit -m 'sync' && git pom && cd -"
+
+alias pgadmin="/home/forrest/Workspace/anaconda3/bin/python /home/forrest/Workspace/anaconda3/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py"

7aec1a504b1e119238e4e7e978dce4114e777955
Forrest Desjardins <desjardinsfg@gmail.com>
2017-12-28 12:18:59 -0900

    sync

--- dotfiles/.zsh/startup.zsh
+++ dotfiles/.zsh/startup.zsh
@@ -3,38 +3,37 @@ P=(
   'Steven Wright'
   'George Carlin'
   'Mitch Hedberg'
-
   'Steve Jobs'
   'Bill Gates'
   'Elon Musk'
   'Jed McCaleb'
-
   'Richard Feynman'
   'Albert Einstein'
   'Donald Knuth'
   'Alan Turing'
-
   'Noam Chomsky'
   'Bill Nye'
   'Neil deGrasse Tyson'
   'Adam Smith'
-
   'Nelson Mandela'
   'Mary Lasker'
   'Elie Wiesel'
   'Eleanor Roosevelt'
-
   'Barack Obama'
   'Joe Biden'
   'Vladimir Putin'
   'Abraham Lincoln'
-
   'John Lennon'
   'Stephen King'
   'Christopher Hitchens'
   'Hunter S. Thompson'
 )
 
-wikiquote random ${P[$(( $RANDOM % ${#P[@]} + 1 ))]} | cowsay -W 60
+# for person in $P
+# do
+#   wikiquote cache add $person
+# done
+
+wikiquote random ${P[$(( $RANDOM % ${#P[@]} + 1 ))]} -m 240 | cowsay -W 60
 
 source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"

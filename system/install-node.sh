99747d32896f9bbe1aeafed75e70e67dbced007b
Forrest Desjardins <desjardinsfg@gmail.com>
2018-01-07 06:00:01 -0900

    sync

--- system/install-node.sh
+++ system/install-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install nvm for managing nodejs
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -22,7 +22,11 @@ npmPackages=(
   "tldr"
   "webpack"
   "wikiquote-cli"
-  "yarn"
 )
 
 npm install -g "${npmPackages[@]}"
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
